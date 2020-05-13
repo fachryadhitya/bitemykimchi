c19d4491a8bd8a0d58fd56e085d532946f10cc90
fachryadhitya <fachryadhitya@gmail.com>
2020-05-13 15:05:27 +0700

    adding details section

---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "TteokBokki",
+      category: "Noodle",
+      images: "/images/d1.jpg",
+      price: "Rp 30000",
+      brand: "Korean Food",
+      rating: "4.5",
+      numReviews: 10,
+    },
+    {
+      _id: "2",
+      name: "Kimchi",
+      category: "Fried snack",
+      images: "/images/d1.jpg",
+      price: "Rp 35000",
+      brand: "Korean Food",
+      rating: "4.5",
+      numReviews: 10,
+    },
+    {
+      _id: "3",
+      name: "Odeng",
+      category: "Fried snack",
+      images: "/images/d1.jpg",
+      price: "Rp 39000",
+      brand: "Korean Food",
+      rating: "4.5",
+      numReviews: 10,
+    },
+  ],
+};
