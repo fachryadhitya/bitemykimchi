4ae8f6c8c02fe321a8ac7125154910dca3704c2c
fachryadhitya <fachryadhitya@gmail.com>
2020-05-13 20:45:03 +0700

    using redux

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,31 @@
-import React from "react";
-import data from "../data";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
-const HomeScreen = (props) => {
+function HomeScreen(props) {
+  
+  const productList = useSelector(state => state.productList)
+  const {products, loading, error} = productList
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+     dispatch(listProducts());
+
+    return () => {
+        //
+    };
+  }, []);
+
   return (
+    loading ? <div>Loading</div> : 
+    error ? <div>{error}</div> :
+    
     <ul className="products">
-      {data.products.map((product) => {
+      {products.map((product) => {
         return (
-          <li>
+          <li key={product._id}>
             <div className="product">
               <Link to={"/product/" + product._id}>
                 <img
@@ -31,6 +49,6 @@ const HomeScreen = (props) => {
       })}
     </ul>
   );
-};
+}
 
 export default HomeScreen;
